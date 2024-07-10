d8e1979073beeddae22e398e11732538d6f50816
JonathanLu2005 <jonathanlu2005@yahoo.com>
2024-07-10 21:40:43 +0100

    Everything wroks now? Please?

--- app.py
+++ app.py
@@ -1,71 +1,97 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
-from dataAnalysis import initialiseDataFrame
+# Import libraries
+from flask import Flask, render_template, request, redirect, url_for, flash, send_file
+from dataAnalysis import initialiseDataFrame, analyseData
 from werkzeug.utils import secure_filename
-import shutil
+import io
+import os
 
-UPLOAD_FOLDER = "uploads" 
+# Uploaded files goes to uploads and only accept excel spreadsheets
+UPLOAD_FOLDER = "uploads"  
 ALLOWED_EXTENSIONS = {"xlsx"}
 
+# Variables to hold the analysis for insights
+geoVar = None
+tempoVar = None
+dealTypeVar = None
+
+# Create flask app
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.secret_key = 'supersecretkey'
 
+# Ensure it has the right extension
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def clear_uploads_folder():
-    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
-
-@app.route("/", methods=["POST", "GET"])
+# Home page
+@app.route("/", methods=["POST","GET"])
 def homePage():
+    # Globalise variables to change and use later on
+    global geoVar
+    global tempoVar
+    global dealTypeVar
+
+    # If receive a file (POST)
     if request.method == 'POST':
+
+        # Verifying if file has been uploaded
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
+        
+        # Holds all file
         file = request.files['file']
+
+        # If no file sent, allows user to re submit
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+        
+        # If file has been sent and has right extension
         if file and allowed_file(file.filename):
             try:
-                clear_uploads_folder()
-                filename = secure_filename(file.filename)
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                file.save(file_path)
-                print(f"File saved at: {file_path}")
+                # Clean and prepare file as a dataframe
+                df = initialiseDataFrame(file)
 
-                df = initialiseDataFrame(file_path)
-                result_filename = "Result.xlsx"
-                result_path = os.path.join(app.config['UPLOAD_FOLDER'], result_filename)
-                df.to_excel(result_path, index=False)
-                print(f"Result file saved at: {result_path}")
-                return redirect(url_for('insightPage', filename=result_filename))
+                # Headings for statistics of each category
+                stat_headings = ["Transaction Count", "Transaction Value Mean", "Transaction Value STD", "Transaction Value Min", "Transaction Value Max"]
+                
+                # Calls analyseData in dataAnalysis to get the relevant processed data into format for tables
+                geoVar = analyseData(df, "geographicalRegion", ["Geographical Region"] + stat_headings)
+                
+                tempoVar = analyseData(df, "declaredDate", ["Month"] + stat_headings)
+
+                dealTypeVar = analyseData(df, "dealAttributes", ["Deal Type"] + stat_headings)
+
+                # Go to insightPage app route to put the results onto tables
+                return redirect(url_for('insightPage', filename=secure_filename(file.filename)))
             except Exception as e:
+                # Otherwise if cannot be processed, return the exception error caught
                 return render_template("home.html", errorMessage=f"An error occurred with the file given: {e}. Please submit another spreadsheet of the right format.")
     
+    # If no post method, then return the generic home page
     return render_template("home.html")
 
+# For the analysis
 @app.route("/insights/<filename>")
 def insightPage(filename):
-    return render_template("insights.html", filename=filename)
+    # Receive results from home, then return the insights page with the provided data analysed
+    return render_template("insights.html", filename=filename, geoTable=geoVar.to_html(),\
+                            tempoTable=tempoVar.to_html(), dealTypeTable=dealTypeVar.to_html())
 
-@app.route("/download/<filename>")
-def downloadFile(filename):
+# Allows users to download file
+@app.route("/download/")
+def downloadFile():
+    # Retrieve result from uploads and provide to user
     try:
-        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-        print(f"Downloading file from: {file_path}")
-        return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=True)
+        with open(os.path.join(app.config["UPLOAD_FOLDER"], "result.xlsx"), "rb") as f:
+            return send_file(io.BytesIO(f.read()), as_attachment=True, download_name="result.xlsx")
+
+    # If error then return exception
     except Exception as e:
         return str(e)
 
+# Ensure app runs
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,5 +1,6 @@
-import numpy as np
+# Import libraries
 import pandas as pd
+from calendar import month_name
 
 # Store country for each currency to convert
 countryHashmap = {
@@ -56,12 +57,94 @@ def initialiseDataFrame(file_name):
     # Create column to store country of origin 
     df["geographicalRegion"] = None
 
+    # Convert the dataframe transaction to GBP
     df = df.apply(convertRowsToPounds, axis="columns")
+
     # New column names for simplicity
-    # print(df.columns)
     df.columns = ["corpActionID", "declaredDate", "infoSource","dealAttributes","transactionAmount","transactionCurrency","geographicalRegion"]
 
     # Return clean excel file 
     df.to_excel("uploads/result.xlsx", index=False)
     
+    # Return clean data frame to work with
     return df
+
+# Analyse the data for each country, time and deal attribute
+def analyseData(df, group_criterion, column_headings):
+    # If we want to get the correlation of time, then convert each date to the month number
+    if group_criterion == "declaredDate":
+        df["declaredDate"] = df["declaredDate"].map(lambda d: int(d.split("-")[1]))
+    
+    # If we want to get deal attributes data, need to get each attribute
+    if group_criterion == "dealAttributes":
+        df = expandDealAttributes(df)
+        
+    # Put those groups of data to analyse (deal, time, country) to a group
+    group = df.groupby(group_criterion)
+
+    # Able find the following data with Pandas methods
+    num_transactions = group.size()
+    avg_transactions = group.transactionAmount.mean()
+    std_transactions = group.transactionAmount.std()
+    min_transaction = group.transactionAmount.min()
+    max_transaction = group.transactionAmount.max()
+
+    # New data frame for results
+    df = pd.DataFrame({"0":num_transactions , "1":avg_transactions, "2":std_transactions, "3":min_transaction, "4":max_transaction})
+    
+    # Put the months in chronological order and convert the month number to its value e.g. 1=January, 2=February ...
+    if group_criterion == "declaredDate":
+        df.sort_index(inplace=True)
+        df.index = df.index.map(lambda x: month_name[x])
+    
+    # Remove awkward initial formatting, for a more natural presentation in a table
+    if group_criterion == "dealAttributes":
+        df.index = df.index.map(lambda s: s.strip("\"").replace("_", " "))
+
+    # Format all numbers to 2.d.p and replace Null std with 0
+    df["1"] = df["1"].map(lambda x: "{:.2f}".format(round(x, 2)))
+    df["2"] = df["2"].map(lambda x: "{:.2f}".format(round(x, 2)) if not pd.isnull(x) else "0.00") 
+    df["4"] = df["4"].map(lambda x: "{:.2f}".format(round(x, 2)))
+
+    # Writes the name of the index column in the top-left corner
+    df.columns = column_headings[1:]
+    df.rename_axis("", inplace=True)
+    df.rename_axis(column_headings[0], axis="columns", inplace=True)
+    
+    # Return result of results
+    return df
+
+# Extract each of the attributes to extend the DF so each attribute has its own row instead of being an array
+def expandDealAttributes(df):
+    # Get deal attributes columns
+    deal_attributes_col = df.dealAttributes
+
+    # Empty DF to write changes but use existing DF column names
+    modified_df = pd.DataFrame(columns=df.columns)
+
+    # Track current row in new DF
+    modified_index = 0
+    for index, attributes in deal_attributes_col.items():
+        # List of attributes from string to list
+        attributes = attributes.strip("[]").split(",")
+
+        # Current row of existing DF
+        temp = df.loc[index]
+
+        # New row in DF for each attribute
+        for att in attributes:
+            # Ensure rows are the deal attributes
+            temp.dealAttributes = att
+
+            # New row to data frame and append to next rows
+            modified_df.loc[modified_index] = temp
+            modified_index += 1
+    
+    # Return new DF
+    return modified_df
+
+if __name__ == "__main__":
+    df = initialiseDataFrame("CorporateActionsData.xlsx")
+    _df = expandDealAttributes(df)
+    deal_df = analyseData(_df, "dealAttributes", ["Deal Type", "Transaction Count",\
+         "Transaction Value Mean", "Transaction Value STD", "Transaction Value Min", "Transaction Value Max"])
